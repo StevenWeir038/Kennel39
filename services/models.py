e0d5dadc6ae96e958166ec0a5e5893a968c86e78
StevenWeir038 <weir_steven@btinternet.com>
2022-03-24 16:11:57 +0000

    Create initial services model and migrate

--- services/models.py
+++ services/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Services(models.Model):
+    """
+    Services model
+    """
+    service_name = models.CharField(max_length=20, null=False, blank=False)
+    service_desc = models.TextField(max_length=255, null=False, blank=False)
+
+    def _str__(self):
+        return self.service_name
