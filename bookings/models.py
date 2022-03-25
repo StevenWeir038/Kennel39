320429b1d4afdb8e0d084096690e367b4950787f
StevenWeir038 <weir_steven@btinternet.com>
2022-03-25 15:52:38 +0000

    Add booking model and migrate

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from services.models import Services
 
-# Create your models here.
+
+class Booking(models.Model):
+    """
+    Booking model
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    pet_name = models.CharField(max_length=15, null=False, blank=False)
+    service_type = models.ForeignKey(Services, on_delete=models.CASCADE)
+    booking_time = models.DateTimeField(null=False, blank=False, unique=True)
+
+    class Meta:
+        """ Order bookings by date """
+        ordering = ['booking_time']
+
+    def __str__(self):
+        return str(self.user)
