4adfe7620af5e8b450b14e0434519e1422333c2b
StevenWeir038 <weir_steven@btinternet.com>
2022-03-25 15:57:52 +0000

    Add booking model to admin

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Booking  # from current directory model file import Profile model
 
-# Register your models here.
+# Register your models here
+admin.site.register(Booking)
