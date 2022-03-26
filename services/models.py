f4dbd37e90d2ecfdb533e739b23cae77c62d0c4b
StevenWeir038 <weir_steven@btinternet.com>
2022-03-26 14:24:05 +0000

    Update services model

--- services/models.py
+++ services/models.py
@@ -5,6 +5,7 @@ class Services(models.Model):
     """
     Services model
     """
+    service_id = models.AutoField(primary_key=True)
     service_name = models.CharField(max_length=20, null=False, blank=False)
     service_desc = models.TextField(max_length=255, null=False, blank=False)
 
