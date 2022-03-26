067166c389474531ca909bdcac26ea3a9d150985
StevenWeir038 <weir_steven@btinternet.com>
2022-03-26 17:40:58 +0000

    Create new postgres database, old one faulty

--- env.py
+++ env.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ['DATABASE_URL'] = "postgres://ucriexsgdgampb:ee907c6db7cc840337c517756978e19198aaf6b4b730c4894cbcb6f05b7952b5@ec2-52-209-185-5.eu-west-1.compute.amazonaws.com:5432/d9uso12g20s1mr"
+os.environ['DATABASE_URL'] = "postgres://hhaodwyhethgar:bcdcf1e32de56cd0effbc7a519a7947a92edacc42ae9e6612b1a7644ca85b00e@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/d67iolkqnc7b4l"
 os.environ['CLOUDINARY_URL'] = "cloudinary://483793338797938:3fwAiImp_hqf1uQ2S2nsRUj79X8@dcozzptpp"
 os.environ['SECRET_KEY'] = 'MySecretKey!'
 os.environ['DEVELOPMENT'] = 'True'
