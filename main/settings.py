d6cd75f9f109d60c2ddccefb879bf383457b0a00
StevenWeir038 <weir_steven@btinternet.com>
2022-03-28 16:55:18 +0000

    Install crispy forms

--- main/settings.py
+++ main/settings.py
@@ -60,6 +60,8 @@ SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
