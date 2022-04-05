f9028599369620edd3b089fb1b0c77605daf64de
StevenWeir038 <weir_steven@btinternet.com>
2022-04-05 10:39:05 +0000

    Make cancel btn in view_booking temp, wire up urls

--- bookings/urls.py
+++ bookings/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.view_booking, name='view_booking'),
     path('create_booking/', views.create_booking, name='create_booking'),
     path('edit_booking/<int:booking_id>/', views.edit_booking, name='edit_booking'),
+    path('cancel_booking/<int:booking_id>/', views.cancel_booking, name='cancel_booking'),
 ]
