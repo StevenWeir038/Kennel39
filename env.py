b21926c1a07db31a9223c531e10925f569474d78
StevenWeir038 <weir_steven@btinternet.com>
2022-04-06 08:34:15 +0000

    Remove env variables

--- env.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.environ['DATABASE_URL'] = "postgres://hhaodwyhethgar:bcdcf1e32de56cd0effbc7a519a7947a92edacc42ae9e6612b1a7644ca85b00e@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/d67iolkqnc7b4l"
-os.environ['CLOUDINARY_URL'] = "cloudinary://483793338797938:3fwAiImp_hqf1uQ2S2nsRUj79X8@dcozzptpp"
-os.environ['SECRET_KEY'] = 'MySecretKey!'
-os.environ['DEVELOPMENT'] = 'True'
