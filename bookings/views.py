6a7779afca231368684fab581feac4a5465dc954
StevenWeir038 <weir_steven@btinternet.com>
2022-04-08 14:09:05 +0000

    Restrict booking creation to auth user - imports

--- bookings/views.py
+++ bookings/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from .models import Booking
 from .forms import BookingForm
 
