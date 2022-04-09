7feb21921e56eef243961c6b74d59f5f36dbc083
StevenWeir038 <weir_steven@btinternet.com>
2022-04-09 19:21:19 +0000

    Build a datepicker/filter for view table with js

--- static/js/script.js
+++ static/js/script.js
@@ -11,3 +11,18 @@ $(document).ready(function() {
     $( "#datepicker" ).datepicker("option", "showAnim", "fold");
     $( "#datepicker" ).datepicker("setDate", '0');
 });
+
+
+// live filter of datepicker
+const datepicker = document.getElementById("datepicker");
+const filterResults = (e) => {
+    let resultRows = document.querySelectorAll(".booking-row");
+    let filterData = datepicker.value;
+    resultRows.forEach(row => {
+        row.classList.add("d-none");
+        if (row.innerText.includes(filterData)) {
+            row.classList.remove("d-none");
+        }
+    });
+}
+$(datepicker).on("change", filterResults);
