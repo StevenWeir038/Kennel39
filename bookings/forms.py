0b4be0a1f0572b7a1e664eb56deef59f810863e9
StevenWeir038 <weir_steven@btinternet.com>
2022-04-10 18:59:00 +0000

    Apply jquery datepicker to edit booking model form

--- bookings/forms.py
+++ bookings/forms.py
@@ -6,3 +6,7 @@ class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = ['pet_name', 'date', 'start_time', 'service_type']
+        # Apply date picker direct to a rendered modelForm https://code-examples.net/en/q/f993c1
+        widgets = {
+            'date': forms.DateInput(attrs={'class':'datepicker'})
+            }
--- static/js/script.js
+++ static/js/script.js
@@ -7,9 +7,9 @@
  * Limit date range from today + 12 months
 */
 $(document).ready(function() {
-    $( "#datepicker" ).datepicker({ dateFormat: "MM d, yy", minDate: +0, maxDate: "+12M +0D" });
-    $( "#datepicker" ).datepicker("option", "showAnim", "fold");
-    $( "#datepicker" ).datepicker("setDate", '0');
+    $( ".datepicker" ).datepicker({ dateFormat: "MM d, yy", minDate: +0, maxDate: "+12M +0D" });
+    $( ".datepicker" ).datepicker("option", "showAnim", "fold");
+    $( ".datepicker" ).datepicker("setDate", '0');
 });
 
 
