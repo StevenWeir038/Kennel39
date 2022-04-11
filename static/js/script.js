e16fbcd12744f985493661c4ab230e077ce794b6
StevenWeir038 <weir_steven@btinternet.com>
2022-04-11 11:33:23 +0000

    Revert js datepicker to id instead of class

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,16 @@
 /* jshint esversion: 8, jquery: true */
 
 
+
 /**
  * Datepicker widget
  * Long date format
  * Limit date range from today + 12 months
 */
 $(document).ready(function() {
-    $( ".datepicker" ).datepicker({ dateFormat: "MM d, yy", minDate: +0, maxDate: "+12M +0D" });
-    $( ".datepicker" ).datepicker("option", "showAnim", "fold");
-    $( ".datepicker" ).datepicker("setDate", '0');
+    $( "#datepicker" ).datepicker({ dateFormat: "MM d, yy", minDate: +0, maxDate: "+12M +0D" });
+    $( "#datepicker" ).datepicker("option", "showAnim", "fold");
+    $( "#datepicker" ).datepicker("setDate", '0');
 });
 
 
