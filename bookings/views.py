6b4e11514d69b296172681b7c324fe64fff64c77
StevenWeir038 <weir_steven@btinternet.com>
2022-04-22 15:15:33 +0000

    Format msg output and pstop dit booking clashes

--- bookings/views.py
+++ bookings/views.py
@@ -6,7 +6,9 @@ from .models import Booking
 from .forms import BookingForm
 from datetime import datetime
 
-num_staff = 2
+
+# The number of appointments that can be taken at any given time
+num_staff = 1
 
 
 def view_booking(request):
@@ -33,14 +35,16 @@ def create_booking(request):
             req_date = form.cleaned_data['date']
             req_time = form.cleaned_data['start_time']
             pet_name = form.cleaned_data['pet_name']
+            msg_req_date = req_date.strftime("%A %d, %B, %Y")
+            msg_req_time = req_time.strftime("%-I%p")
             num_same_bookings = Booking.objects.filter(date=req_date, start_time=req_time).count()
-            if num_same_bookings > num_staff:
-                messages.error(request, 'No appointment is available for this time')
+            if num_same_bookings >= num_staff:
+                messages.error(request, f'No appointment is available on {msg_req_date} at {msg_req_time}.')
                 return redirect('view_booking')
             else:
                 form.instance.user = user
                 form.save()
-                messages.success(request, f'Your appointment for {pet_name} has been confirmed for {req_date} at {req_time}.')
+                messages.success(request, f'Your appointment for {pet_name} has been confirmed.')
                 return redirect('view_booking')
     form = BookingForm()
     context = {
@@ -61,9 +65,20 @@ def edit_booking(request, booking_id):
     if request.method == 'POST':
         form = BookingForm(request.POST, instance=booking)
         if form.is_valid():
+
             # will need same logic here as in create booking to prevent booking clashes.
-            form.save()
-            return redirect('view_booking')
+            req_date = form.cleaned_data['date']
+            req_time = form.cleaned_data['start_time']
+            pet_name = form.cleaned_data['pet_name']
+            msg_req_date = req_date.strftime("%A %d, %B, %Y")
+            msg_req_time = req_time.strftime("%-I%p")
+            num_same_bookings = Booking.objects.filter(date=req_date, start_time=req_time).count()
+            if num_same_bookings >= num_staff:
+                messages.error(request, f'No appointment is available on {msg_req_date} at {msg_req_time}.')
+                return redirect('view_booking')
+            else:
+                form.save()
+                return redirect('view_booking')
     form = BookingForm(instance=booking)
     context = {
         'form': form
