27757eda37f0f3fcc40de4fcfedd56fb5e968fff
StevenWeir038 <weir_steven@btinternet.com>
2022-04-28 19:22:48 +0000

    Check heroku deployment

--- main/settings.py
+++ main/settings.py
@@ -17,7 +17,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['kennel39.herokuapp.com', 'localhost']
 
