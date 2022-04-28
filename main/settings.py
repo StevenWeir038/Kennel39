d2f9e2a60615ee5fc6e20d2fc00d3b1de9e81b1f
StevenWeir038 <weir_steven@btinternet.com>
2022-04-28 19:50:00 +0000

    Update database settings

--- main/settings.py
+++ main/settings.py
@@ -17,8 +17,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['kennel39.herokuapp.com', 'localhost']
 
@@ -95,18 +94,18 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 if os.environ.get("DEVELOPMENT"):
-    # Heroku database
-    print("database = PostgreSQL via Heroku")
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get("DATABASE_URL")),
-        }
-else:
     # Testing database
     print("database = db.sqlite3")
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
+        }
+else:
+    # Heroku database
+    print("database = PostgreSQL via Heroku")
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL")),
         }
     }
 
